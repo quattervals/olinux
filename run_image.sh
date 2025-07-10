e9c61178c301093d971a79625e419fd2c860553a
Oliver With <oliverwith@bbv.ch>
2025-07-10 09:32:28 +0200

    Work with DHCP
    Add script to always assign the same IP address to mac of device

--- run_image.sh
+++ run_image.sh
@@ -15,7 +15,7 @@ qemu-system-arm \
     -kernel zImage \
     -drive id=disk0,file=beaglebone-image-qemuarm.rootfs.ext4,if=none,format=raw \
     -device virtio-blk-device,drive=disk0 \
-    -netdev user,id=net0,net=192.168.1.100/24,host=192.168.1.2,hostfwd=tcp::2222-:22 \
+    -netdev user,id=net0,hostfwd=tcp::2222-:22 \
     -device virtio-net-device,netdev=net0 \
     -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-pci,rng=rng0 \
     -device virtio-rng-device,rng=rng0 \
